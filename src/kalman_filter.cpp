a4d05b94fdb1df0549e33148b55eb501fa02a655
mdevana <31383760+mdevana@users.noreply.github.com>
2020-12-25 07:08:59 +0100

    Kalman Filter update

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -34,11 +34,11 @@ void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
-   /*VectorXd y = z - H_ * x_ ;
+   VectorXd y = z - H_ * x_ ;
    MatrixXd S = H_ * P * H_.transpose() + R_ ;
    MatrixXd K = P_ * H_.transpose() * S.inverse();
    
-   I = MatrixXd::Identity(2, 2);
+   /*I = MatrixXd::Identity(2, 2);
    
    x_ = x_ + ( K * y) ;
    P = (I - K * H_) * P_;*/
