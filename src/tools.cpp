8a7a423efe5f903262251b29c7cd99a99f456176
mdevana <31383760+mdevana@users.noreply.github.com>
2020-12-25 06:23:02 +0100

    cout added

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,6 +14,41 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // TODO: YOUR CODE HERE
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  
+  /*if ((estimations.size()==0) || (estimations.size()!=ground_truth.size())){
+    cout<<"invalid inputs";
+    return(rmse);
+  }
+    
+  // TODO: accumulate squared residuals
+  //cout<<estimations[0]<<endl;
+  VectorXd a,b;
+  VectorXd c;
+  for (int i=0; i < estimations.size(); ++i) {
+    // ... your code here
+    a=estimations[i];
+    b=ground_truth[i];
+    c=a-b;
+    c=(c.array()*c.array());
+    rmse+=c;
+    
+  }
+
+  // TODO: calculate the mean
+    rmse=rmse/estimations.size();
+
+  // TODO: calculate the squared root
+  rmse=rmse.array().sqrt();
+
+  // return the result*/
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +56,27 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+   MatrixXd Hj=MatrixXd(3,4);
+  // recover state parameters
+  /*float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);*/
+
+  // TODO: YOUR CODE HERE 
+
+  // check division by zero
+  /*float den=px*px+py*py;
+  if (den==0) {
+      cout<<"dividion by Zero"<<endl;
+      return (Hj);
+  }
+  
+  
+  // compute the Jacobian matrix
+  Hj<< px/(sqrt(den)),py/(sqrt(den)),0,0,
+       -py/den, px/den,0,0,
+       py*(vx*py-vy*px)/(den*sqrt(den)),py*(vy*px-vx*py)/(den*sqrt(den)),px/sqrt(den),py/sqrt(den);*/
+
+  return Hj;
 }
